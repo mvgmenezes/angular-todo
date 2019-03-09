9273d617770accb5539669d32e6a7e7bff3f7a2b
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-09 00:13:08 -0300

    Criando o projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
+//the @component  é com se fosse o decorator
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //selector - é a tag name desse componente, caso alguem queria usa-lo Ex: <app-root></app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
