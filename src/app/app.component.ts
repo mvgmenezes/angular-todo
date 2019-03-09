878a949b7feb146174e465cc68e97263351b86d0
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-09 00:19:30 -0300

    Trabalhando com as variavies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 //the @component  é com se fosse o decorator
 @Component({
   selector: 'app-root', //selector - é a tag name desse componente, caso alguem queria usa-lo Ex: <app-root></app-root>
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', //template do componente, localizacao do html
+  styleUrls: ['./app.component.css'] //style do componente, css 
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'todo'; //variavel que pode ser usada dentro do componente.html atraves de {{ title }}
+  message = 'Welcome to World'
 }
