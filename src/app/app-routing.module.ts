6c61378aa72836de7373c5154de44e156cb83438
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-09 11:11:10 -0300

    Trabalhando com rotas, e criando uma de erro generica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+//welcome
+const routes: Routes = [
+  { path: '', component: LoginComponent}, //path principal direciona para Login
+  { path: 'login', component: LoginComponent},
+  { path: 'welcome', component: WelcomeComponent},
+  { path: '**', component: ErrorComponent} //para todas as outras rotas(**) apresenta a tela de erro 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    LoginComponent //quando se cria um novo componente (@Component) deve ser associado como um angular modulo(@NgModule)
+    LoginComponent,
+    ErrorComponent //quando se cria um novo componente (@Component) deve ser associado como um angular modulo(@NgModule)
   ],
   imports: [
     BrowserModule,
