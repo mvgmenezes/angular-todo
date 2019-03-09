ee73523788f147004e015df4a22d31b7f48fc9af
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-09 15:49:48 -0300

    Implementando a verificacao se o usuario está logado para habilitar os itens de menu. usando o sessionstorage and service

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -6,12 +6,22 @@ import { Injectable } from '@angular/core';
 })
 export class HardcodedAuthenticationService {
 
+  
   constructor() { }
 
   autenticar(username, password) :boolean{
+    console.log('before ' + this.isUserLoggedIn());
     if (username==="mmenezes" && password==="123"){
+      //salvando o usuario na session do browser
+      sessionStorage.setItem('authenticaterUser', username); //https://www.w3schools.com/jsref/prop_win_sessionstorage.asp
+      console.log('after ' + this.isUserLoggedIn());
       return true;
     }
     return false;
+  }
+
+  isUserLoggedIn(){
+    let user = sessionStorage.getItem('authenticaterUser');
+    return !(user == null);
   }
 }
