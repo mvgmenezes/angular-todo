3b93a2020577efeafae6e48a98749d1dc3084c12
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-09 00:39:26 -0300

    Alterando o CSS do componente e refletindo na view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 //the @component  é com se fosse o decorator
 @Component({
   selector: 'app-root', //selector - é a tag name desse componente, caso alguem queria usa-lo Ex: <app-root></app-root>
-  template: '<h1> {{ title}} </h1>', //template do componente, localizacao do html
+  templateUrl: './app.component.html', //template do componente, localizacao do html
+  //template: '<h1> {{ title}} </h1>', //incluindo o template diretamente
   styleUrls: ['./app.component.css'] //style do componente, css 
 })
 export class AppComponent {
