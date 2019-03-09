346b907b50689cc647310545b9e451212584b7e9
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-09 13:55:46 -0300

    Criando uma classe interna e usando no ngFor para apresentação, e formatando a data com pipe

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+  
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ){
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,16 +20,17 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    { id : 1, description: 'Learn to dance'},
-    { id : 2, description: 'Become an Expert in Angular'},
-    { id : 3, description: 'Visit India'},
-    { id : 4, description: 'Become a fluent English'}
+    new Todo(1,'Learn to dance', false, new Date()),
+    new Todo(2,'Become an Expert in Angular', false, new Date()),
+    new Todo(3,'Visit India', false, new Date()),
+    new Todo(4,'Become a fluent English', false, new Date())
   ]
 
-  /* todo = {
-    id : 1,
-    description: 'Learn to dance'
-  } */
+  /* todos = [
+      { id : 1, description: 'Learn to dance'},
+      { id : 2, description: 'Become an Expert in Angular'},
+      { id : 3, description: 'Visit India'}
+   ] */
   constructor() { }
 
   ngOnInit() {
