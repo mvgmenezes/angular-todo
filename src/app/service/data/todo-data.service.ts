cecb5a8cb14c0991e5724c6d789063cafa727be2
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-09 21:45:08 -0300

    Impementando o update todo, recuperando os dados por id para poder alterar

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -19,7 +19,7 @@ export class TodoDataService {
     return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`)
   }
 
-  updateTodo(username, id){
-    return this.http.update(`http://localhost:8080/users/${username}/todos/${id}`)
+  retrieveTodo(username, id){
+    return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`)
   }
 }
