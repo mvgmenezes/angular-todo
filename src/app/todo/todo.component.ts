eddf3d7d9911b05378f3ef8e02aa550eeec21328
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-09 22:44:17 -0300

    Implementado o update chamando a API

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoDataService } from '../service/data/todo-data.service';
 import { Todo } from '../list-todos/list-todos.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
@@ -15,7 +15,8 @@ export class TodoComponent implements OnInit {
 
   constructor(
     private todoService: TodoDataService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -28,7 +29,13 @@ export class TodoComponent implements OnInit {
   }
 
   saveTodo(){
-
+    this.todoService.updateTodo('mmenezes', this.id, this.todo)
+      .subscribe(
+        data => {
+          console.log(data)
+          this.router.navigate(['todos'])
+        }
+      )
   }
 
 }
