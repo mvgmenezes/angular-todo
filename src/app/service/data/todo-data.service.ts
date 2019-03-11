3dd4a2f119f779333afbdf12d8e835743e0c3058
Marcus Menezes <mvgmenezes@gmail.com>
2019-03-11 17:02:59 -0300

    Acertando o delete e update da API com JPA

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from 'src/app/list-todos/list-todos.component';
-import { API_URL, TODO_JPA_API_URL } from 'src/app/app.constants';
+import { TODO_JPA_API_URL } from 'src/app/app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +18,7 @@ export class TodoDataService {
   }
 
   deleteTodo(username, id){
-    return this.http.delete(`${API_URL}/users/${username}/todos/${id}`)
+    return this.http.delete(`${TODO_JPA_API_URL}/users/${username}/todos/${id}`)
   }
 
   retrieveTodo(username, id){
@@ -27,12 +27,12 @@ export class TodoDataService {
   }
 
   updateTodo(username, id, todo){
-    return this.http.put(`${API_URL}/users/${username}/todos/${id}`, 
+    return this.http.put(`${TODO_JPA_API_URL}/users/${username}/todos/${id}`, 
       todo);// apos a url é o que será enviado no body
   }
 
   createTodo(username, todo){
-    return this.http.post(`${API_URL}/users/${username}/todos`, 
+    return this.http.post(`${TODO_JPA_API_URL}/users/${username}/todos`, 
       todo);// apos a url é o que será enviado no body
   }
 }
